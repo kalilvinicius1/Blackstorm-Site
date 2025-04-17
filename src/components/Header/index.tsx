a54f03a6e6980abb488a7b68c4b7383970189120
kalilvinicius1 <kalilvinicius23@gmail.com>
2025-04-17 10:05:29 -0400

    Primeiro Commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+
+function Header(){
+    return(
+        <header className="container">
+            <h1>BLACKSTORM</h1>
+            <div className="menu-bar">
+                
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+import './styles.css'
+
+function Main(){
+    return(
+        <div className='container'>
+            <Header />
+        </div>
+    )
+}
+
+export default Main;
